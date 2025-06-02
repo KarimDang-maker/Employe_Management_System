1a16caa22c7b700c080494ad8f4d0314fdd368ca
geniusroot <geniusrootkali@gmail.com>
2025-06-02 21:11:27 +0100

    Developpement de la couche DTO

---
+++ src/main/java/root/store/store/DTO/EmployeDTO.java
@@ -0,0 +1,19 @@
+package root.store.store.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import root.store.store.Model.TypePoste;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record EmployeDTO(
+        UUID id,
+        String nom,
+        String email,
+        Integer anciennete,
+        Integer salaire,
+        TypePoste poste,
+        String departement
+){}
